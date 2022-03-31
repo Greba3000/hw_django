9635b471ecc1ff23aea623e1da10812839578934
Greba3000 <greba3000@gmail.com>
2022-03-31 23:47:12 +0300

    Completed 07 chapter

--- board/news_app/admin.py
+++ board/news_app/admin.py
@@ -2,11 +2,42 @@ from django.contrib import admin
 from news_app.models import *
 
 
+class CommentInLine(admin.TabularInline):
+    model = Comment
+
+
 @admin.register(News)
 class NewsAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'title', 'created_at', 'updated_at', 'flag']
+    list_filter = ['flag']
+    search_fields = ['title', 'description']  # поиск будет производиться только в указанных полях
+    inlines = [CommentInLine]
+    actions = ['mark_as_active', 'mark_as_not_active']
+
+    def mark_as_active(self, request, queryset):
+        queryset.update(flag='True')
+
+    def mark_as_not_active(self, request, queryset):
+        queryset.update(flag='False')
+
+    mark_as_active.short_description = 'Опубликовать выбранные Новости'
+    mark_as_not_active.short_description = 'Деактивировать выбранные Новости'
 
 
 @admin.register(Comment)
 class CommentsAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'author', 'get_text_15_len', 'news']
+    list_filter = ['author']
+    actions = ['removed_by_admin']
+
+    def removed_by_admin(self, request, queryset):
+        queryset.update(text='Сообщение удалено админом! Нарушен пункт ...')
+
+    removed_by_admin.short_description = 'Удалить выбранные Комментарии. Пункт ...'
+
+    def get_text_15_len(self, obj):
+        if len(obj.text) > 15:
+            return f'{obj.text[:15]}...'
+        return obj.text
+
+    get_text_15_len.short_description = 'Комментарий'
--- board/news_app/models.py
+++ board/news_app/models.py
@@ -7,21 +7,27 @@ class News(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания',
                                       editable=True)  # без editable=True не отобразит в админке
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата редактирования', editable=True)
-    flag = models.BooleanField(default=False, verbose_name='Флаг активности')
+    flag = models.BooleanField(default=True, verbose_name='Статус публикации')
 
     def __str__(self):
         return self.title
 
     class Meta:
-        ordering = ['created_at']
+        ordering = ['-created_at'] # в обратном порядке "-"
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
 
 
 class Comment(models.Model):
-    author = models.CharField(max_length=50)
-    text = models.CharField(max_length=200)
+    author = models.CharField(max_length=50, verbose_name='Автор')
+    text = models.CharField(max_length=200, verbose_name='Текст комментария')
     news = models.ForeignKey('News', default=None, null=True, on_delete=models.CASCADE,
                              related_name='comment',
                              verbose_name='Новость')  # related new change comment_set for comment
 
     def __str__(self):
         return self.author
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
