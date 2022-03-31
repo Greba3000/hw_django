42ef4dd55841fa32367c67d4e83776041f499a2b
Greba3000 <greba3000@gmail.com>
2022-03-31 11:34:48 +0300

    Technical rework of structure

--- board/board/urls.py
+++ board/board/urls.py
@@ -18,6 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('advertisements.urls')),
     path('', include('news_app.urls')),
 ]
